ff5e8da438509b1a852564143b07aaf39eaa9d8c
aclavel <antoine.clavel@sts-sio-caen.info>
2015-09-28 12:04:37 +0200

    liste de ses articles de la faq par admin

--- app/controllers/Faqs.php
+++ app/controllers/Faqs.php
@@ -2,6 +2,7 @@
 use micro\orm\DAO;
 use micro\views\Gui;
 use micro\js\Jquery;
+use micro\utils\RequestUtils;
 /**
  * Gestion des articles de la Faq
  * @author jcheron
@@ -42,22 +43,19 @@ class Faqs extends \_DefaultController {
 
 
 
-	public function index($params=null){
+	public function index($param=null){
 
 	global $config;
-		$orderBy="";
+		$orderBy="1=1 order by idCategorie";
+		if(!is_array($param) && $param!=null){
+			$param=array($param);
+		}
 		$baseHref=get_class($this);
-		if(isset($params)){
-			if(is_string($params)){
-				$message=new DisplayedMessage($params);
-			}elseif (is_array($params)){
-				if(sizeof($params)>1)
-					$orderBy="1=1 order by ".$params[1];
-				else
-					$message=new DisplayedMessage($params[0]);
-
-			}else 
-				$message=$params;
+		if(isset($param) && $param[0] != "mess"){
+			if(is_string($param[0])){
+				$message=new DisplayedMessage($param[0]);
+			}else
+				$message=$param[0];
 			if(isset($message)){
 				$message->setTimerInterval($this->messageTimerInterval);
 				$this->_showDisplayedMessage($message);
@@ -66,6 +64,11 @@ class Faqs extends \_DefaultController {
 		
 		
 		$objects=DAO::getAll($this->model,$orderBy);
+		
+		if(Auth::isAdmin()){
+			echo "<a class='btn btn-primary' href='".$config["siteUrl"].$baseHref."/mesArticles'>Mes articles</a>";
+			echo "<a class='btn btn-info' href='".$config["siteUrl"].$baseHref."'>Tous les articles</a>";
+		}
 		echo "<table class='table table-striped'>";
 		echo "<thead>";
 			echo "<tr>";
@@ -75,6 +78,7 @@ class Faqs extends \_DefaultController {
 						echo "Trier par... <span class='caret'></span>";
 					echo "</button>";
 					echo "<ul class='dropdown-menu' role='menu'>";
+						echo "<li><a href='Faqs/index'>Sans filtre</a></li>";
 						echo "<li><a href='Faqs/index/mess/idCategorie'>categorie</a></li>";
 						echo "<li><a href='Faqs/index/mess/dateCreation'>date de creation</a></li>";
 					echo "</ul>";
@@ -86,7 +90,8 @@ class Faqs extends \_DefaultController {
 
 		$currentOrder="";
 		$func="getRien";
-			switch ($params[1]){
+		if(count($param)>1){
+			switch ($param[1]){
 				case "idCategorie":
 					$func="getCategorie";
 					break;
@@ -94,6 +99,8 @@ class Faqs extends \_DefaultController {
 					$func="getDateCreation";
 					break;
 			}
+			
+		}
 		if (Auth::isAdmin()){
 			foreach ($objects as $object){
 				
@@ -103,7 +110,7 @@ class Faqs extends \_DefaultController {
 				}
 				
 				echo "<tr>";
-				echo "<td class='titre-faq'><a href='".$baseHref."/frm2/".$object->getId()."' style='color:#253939'><b>".$object->getTitre()."</b> - ".$object->getUser()."</a></td>";
+				echo "<td class='titre-faq' style='width:80%'><a href='".$baseHref."/frm2/".$object->getId()."' style='color:#253939'><b>".$object->getTitre()."</b> - ".$object->getUser()."</a></td>";
 				echo "<td class='td-center'><a class='btn btn-success btn-xs' href='".$baseHref."/frm2/".$object->getId()."'><span class='glyphicon glyphicon-eye-open' aria-hidden='true'></span></a></td>";
 					
 				if (Auth::getUser()==$object->getUser()){
@@ -219,5 +226,67 @@ class Faqs extends \_DefaultController {
 	}
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public function mesArticles($param = NULL) {
+		
+	global $config;
+		$orderBy="1=1 order by idCategorie";
+		$baseHref=get_class($this);		
+		
+		$objects=DAO::getAll($this->model,$orderBy);
+		
+		echo "<a class='btn btn-primary' href='".$config["siteUrl"].$baseHref."/mesArticles'>Mes articles</a>";
+		echo "<a class='btn btn-info' href='".$config["siteUrl"].$baseHref."'>Tous les articles</a>";
+				echo "<br><br><table class='table table-striped'><tbody>";
+			foreach ($objects as $object){
+				if (Auth::getUser()==$object->getUser()){
+					
+					echo "<tr>";
+					echo "<td class='titre-faq' style='width:80%'><a href='".$baseHref."/frm2/".$object->getId()."' style='color:#253939'><b>".$object->getTitre()."</b></a></td>";
+					echo "<td class='td-center'><a class='btn btn-success btn-xs' href='".$baseHref."/frm2/".$object->getId()."'><span class='glyphicon glyphicon-eye-open' aria-hidden='true'></span></a></td>";
+						
+					echo "<td class='td-center'><a class='btn btn-primary btn-xs' href='".$baseHref."/frm/".$object->getId()."'><span class='glyphicon glyphicon-edit' aria-hidden='true'></span></a></td>";
+					if ($object->getDisable()=="0"){
+						echo "<td class='td-center'><a class='btn btn-warning btn-xs' href='".$baseHref."/disable/".$object->getId()."'><span class='glyphicon glyphicon-pause' aria-hidden='true'></span></a></td>";
+					}
+					else {
+						echo "<td class='td-center'><a class='btn btn-info btn-xs' href='".$baseHref."/activate/".$object->getId()."'><span class='glyphicon glyphicon-play' aria-hidden='true'></span></a></td>";
+					}
+					echo "<td class='td-center'><a class='btn btn-danger btn-xs' href='".$baseHref."/delete/".$object->getId()."'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td>";
+				}
+			}
+			echo "</tr>";
+		
+		echo "</tbody>";
+		echo "</table>";
+		
+		if (Auth::isAdmin()){
+			echo "<a class='btn btn-primary' href='".$config["siteUrl"].$baseHref."/frm'>Ajouter...</a>";
+		}
+	}
 
 }
