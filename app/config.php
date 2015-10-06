a873f7e7c19ca2fcdf41047b91fc49fbde8a5b48
aclavel <antoine.clavel@sts-sio-caen.info>
2015-10-06 15:36:09 +0200

    OKLM

--- app/config.php
+++ app/config.php
@@ -1,13 +1,15 @@
 <?php
 return array(
-		"siteUrl"=>"http://127.0.0.1/helpdesk/",
+		"siteUrl"=>"http://127.0.0.1/helpdesk-TripleA/",
 		"documentRoot"=>"DefaultC",
 		"database"=>[
-				"dbName"=>"helpdesk",
+				"dbName"=>"helpdesk-TripleA",
 				"serverName"=>"127.0.0.1",
 				"port"=>"3306",
 				"user"=>"root",
 				"password"=>""
 		],
-		"directories"=>["my"]
+		"directories"=>["my","tests"],
+		"test"=>true
 );
+?>
